7db6153750424e6c34758c8186d02d52e202a067
Espen Trydal <espen.trydal@gmail.com>
2017-01-28 23:45:42 +0100

    adding cargo minor mode

--- init.el
+++ init.el
@@ -14,8 +14,9 @@
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (go rust-mode exec-path-from-shell go-mode))))
+ '(package-selected-packages (quote (cargo go rust-mode exec-path-from-shell go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -36,6 +37,12 @@
 
 (add-to-list 'load-path "/Users/espen/.emacs.d/packages")
 
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
+(add-hook 'rust-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+
+
 (setenv "GOPATH" "/Users/espen/Go")
 (add-to-list 'exec-path "/Users/espen/Go/bin")
 (add-hook 'before-save-hook 'gofmt-before-save)
@@ -51,21 +58,16 @@
       mac-right-command-modifier 'meta
       x-select-enable-clipboard t)
 
-
-
-
 (setq package-build-archive-dir "/Users/espen/.emacs.d/packages")
 
 ;; Load CEDET.
 ;; See cedet/common/cedet.info for configuration details.
 ;; IMPORTANT: For Emacs >= 23.2, you must place this *before* any
 ;; CEDET component (including EIEIO) gets activated by another 
-;; package (Gnus, auth-source, ...).
 ;;(load-file "~/.emacs.d/vendor/cedet/cedet-devel-load.el")
 
 ;; Add further minor-modes to be enabled by semantic-mode.
 ;; See doc-string of `semantic-default-submodes' for other things
-;; you can use here.
 ;(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
 ;(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
 ;(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
@@ -97,7 +99,6 @@
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (set-face-attribute 'default nil :height 160)
-
 
 ;; Lazy swapping buffers with buffer-move.el
 (require 'buffer-move)
